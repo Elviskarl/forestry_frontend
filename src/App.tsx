9a17d6b928d499c176b8de6a1b1c0c1874a67048
elvis <elviskarl343@gmail.com>
2026-07-14 14:12:57 +0300

    Set up react router to handle routing within the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home";
+import Map from "./pages/map";
+
 function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="map" element={<Map />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,32 +1,36 @@
+import { NavLink, Outlet } from "react-router-dom";
 import app_logo_url from "../../assets/logo.jpeg";
 import "./styles/index.css";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <div className="logo-img">
-          <img src={app_logo_url} alt="App Logo" />
+    <>
+      <nav className="navbar">
+        <div className="logo">
+          <div className="logo-img">
+            <img src={app_logo_url} alt="App Logo" />
+          </div>
         </div>
-      </div>
-      <ul className="nav-list" role="list">
-        <li>
-          <a href="#" className="nav-link">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link">
-            Map
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link">
-            About
-          </a>
-        </li>
-      </ul>
-    </nav>
+        <ul className="nav-list" role="list">
+          <li>
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/map" className="nav-link">
+              Map
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className="nav-link">
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
