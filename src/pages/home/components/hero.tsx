9821d55a8eaa16353a7ad1cc7276feb9586a410b
elvis <elviskarl343@gmail.com>
2026-07-14 11:28:59 +0300

    Initialized the home component and styled it

---
+++ src/pages/home/components/hero.tsx
@@ -0,0 +1,17 @@
+// import hero_img_src from "../../../assets/hero.webp";
+import "../styles/index.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1 className="hero-title">
+        Above the canopy: Twenty five years of forest change
+      </h1>
+      <h3 className="hero-subtitle">
+        Tracking the Changing Landscape of the Mau Forest
+      </h3>
+    </div>
+  );
+}
+
+export default Hero;
