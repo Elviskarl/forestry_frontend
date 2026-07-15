7acd619d4346d7a7521db52466dedc365aa0cee3
elvis <elviskarl343@gmail.com>
2026-07-15 19:24:09 +0300

    Added a footer which credits the hero section image in the component and styled it

--- src/pages/home/components/hero.tsx
+++ src/pages/home/components/hero.tsx
@@ -1,15 +1,26 @@
-// import hero_img_src from "../../../assets/hero.webp";
 import "../styles/index.css";
 
 function Hero() {
   return (
     <div className="hero">
-      <h1 className="hero-title">
-        Above the canopy: Twenty five years of forest change
-      </h1>
-      <h3 className="hero-subtitle">
-        Tracking the Changing Landscape of the Mau Forest
-      </h3>
+      <div className="hero-texts">
+        <h1 className="hero-title">Above the canopy</h1>
+        <h3 className="hero-subtitle">
+          Tracking the Changing Landscape of the Mau Forest
+        </h3>
+      </div>
+      <div className="hero-footer">
+        <span className="image-source-link-container">
+          Image credit -{" "}
+          <a
+            href="https://infonile.org/en/2022/10/how-politics-has-subverted-conservation-efforts-to-protect-kenyas-mau-forest/"
+            target="_blank"
+            className="image-source-link"
+          >
+            infonile.org
+          </a>
+        </span>
+      </div>
     </div>
   );
 }
