9d7edbb3dc4eb762f5e4c394e593a5ff0f6292ab
elvis <elviskarl343@gmail.com>
2026-07-24 15:58:15 +0300

    Refactored shapefileContext to use state instead of refs for data management

--- src/context/createShapefileContext.tsx
+++ src/context/createShapefileContext.tsx
@@ -1,10 +1,8 @@
 import type { FeatureCollection, MultiPolygon, Polygon } from "geojson";
-import { createContext, type RefObject } from "react";
+import { createContext } from "react";
 
 interface shapefileContext {
-  mau_forest: RefObject<FeatureCollection<Polygon | MultiPolygon> | null>;
-  target_counties: RefObject<FeatureCollection<Polygon | MultiPolygon> | null>;
+  mau_forest: FeatureCollection<Polygon | MultiPolygon> | null;
+  target_counties: FeatureCollection<Polygon | MultiPolygon> | null;
 }
-export const ShapefileContext = createContext<shapefileContext | null>(
-  null,
-);
+export const ShapefileContext = createContext<shapefileContext | null>(null);
--- src/context/provideShapefileContext.tsx
+++ src/context/provideShapefileContext.tsx
@@ -1,6 +1,6 @@
 import type { FeatureCollection, MultiPolygon, Polygon } from "geojson";
 import { ShapefileContext } from "./createShapefileContext";
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import { loadBoundary } from "../utils/utils";
 
 export function ShapefileContextProvider({
@@ -8,24 +8,18 @@ export function ShapefileContextProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const mau_forest = useRef<FeatureCollection<Polygon | MultiPolygon> | null>(
-    null,
-  );
-  const target_counties = useRef<FeatureCollection<
+  const [mau_forest, set_mau_forest] = useState<FeatureCollection<
+    Polygon | MultiPolygon
+  > | null>(null);
+  const [target_counties, set_target_counties] = useState<FeatureCollection<
     Polygon | MultiPolygon
   > | null>(null);
 
   useEffect(() => {
     async function loadShapefile() {
       try {
-        if (!mau_forest.current) {
-          const data = await loadBoundary("mau_forest_reserve.geojson");
-          mau_forest.current = data;
-        }
-        if (!target_counties.current) {
-          const data = await loadBoundary("target_counties.json");
-          target_counties.current = data;
-        }
+        set_mau_forest(await loadBoundary("mau_forest_reserve.geojson"));
+        set_target_counties(await loadBoundary("target_counties.json"));
       } catch (error) {
         console.error("Error loading shapefile:", error);
       }
