e676bb5e02157afa3c98e780d6994b75024670b9
elvis <elviskarl343@gmail.com>
2026-07-26 19:44:06 +0300

    Add GeoJSON overlays for target counties and forest  collections in LeafletMap component

--- src/pages/map/components/LeafletMap.tsx
+++ src/pages/map/components/LeafletMap.tsx
@@ -1,7 +1,25 @@
-import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
+import { MapContainer, TileLayer, LayersControl, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useContext } from "react";
+import { ShapefileContext } from "../../../context/createShapefileContext";
+import type { StyleFunction } from "leaflet";
 
 function LeafletMap() {
+  const { target_counties, mau_forest } = useContext(ShapefileContext)!;
+  if (!target_counties || !mau_forest) return;
+
+  const counties_Style: StyleFunction = () => ({
+    color: "#2E7D32",
+    weight: 2,
+    fillColor: "#66BB6A",
+    fillOpacity: 0.2,
+  });
+  const forest_Style: StyleFunction = () => ({
+    color: "#000",
+    weight: 2,
+    fillColor: "gray",
+    fillOpacity: 0.2,
+  });
   return (
     <MapContainer zoom={12} scrollWheelZoom={true} center={[-0.55, 35.648]}>
       <LayersControl position="topright">
@@ -18,6 +36,12 @@ function LeafletMap() {
             subdomains={["mt1", "mt2", "mt3"]}
           />
         </LayersControl.BaseLayer>
+        <LayersControl.Overlay name="Counties">
+          <GeoJSON data={target_counties} style={counties_Style} />
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="mau forest">
+          <GeoJSON data={mau_forest} style={forest_Style} />
+        </LayersControl.Overlay>
       </LayersControl>
     </MapContainer>
   );
