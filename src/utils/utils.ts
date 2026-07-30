99c0f704dd04008e78642d293e742f1f54b3a8d3
elvis <elviskarl343@gmail.com>
2026-07-31 00:21:41 +0300

    Removed the commented out fetch call for loadBoundary function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,7 +3,6 @@ import type { FeatureCollection, MultiPolygon, Polygon } from "geojson";
 export async function loadBoundary(
   filename: string,
 ): Promise<FeatureCollection<Polygon | MultiPolygon>> {
-  // const res = await fetch("crack_spots/data/Nairobi_subCounty.json");
   const res = await fetch(`${import.meta.env.BASE_URL}data/${filename}`);
 
   if (!res.ok) throw new Error(`Failed to load boundary: ${res.statusText}`);
