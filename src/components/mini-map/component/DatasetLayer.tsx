43ce7283f53474db1d3eaa8738fbf8aac0b8fc1f
elvis <elviskarl343@gmail.com>
2026-08-21 17:41:57 +0300

    Add collapsible classification legend with dropdown functionality and styled it

--- src/components/mini-map/component/DatasetLayer.tsx
+++ src/components/mini-map/component/DatasetLayer.tsx
@@ -1,37 +1,59 @@
 import { LayersControl, TileLayer } from "react-leaflet";
 import type { ComparisonResponse } from "../../types";
+import downArrowUrl from "../../../assets/down_arrow.svg";
+import { useState } from "react";
 
 interface DatasetLayerProps {
   data: ComparisonResponse["data"];
 }
 function ClassificationLegend() {
+  const [isMinimized, setIsMinimized] = useState(false);
+
+  function handleClick() {
+    setIsMinimized((prevVal) => !prevVal);
+  }
   return (
     <div className="classification-legend">
-      <strong>Legend</strong>
-
-      <div>
-        <span className="legend-color forest" />
-        Forest
+      <div
+        className={`${isMinimized ? "legend-title-container-minimized" : ""} legend-title-container`}
+      >
+        <strong>Legend</strong>
+        <div
+          className={`${isMinimized ? "legend-dropdown-container-minimized" : ""} legend-dropdown-container`}
+          onClick={handleClick}
+          title={isMinimized ? "expand" : "collapse"}
+        >
+          <img src={downArrowUrl} alt="arrow" />
+        </div>
       </div>
 
-      <div>
-        <span className="legend-color herbaceous" />
-        H.vegetation
-      </div>
+      <div
+        className={`${isMinimized ? "legend-classes-container-minimized" : ""} legend-classes-container`}
+      >
+        <div>
+          <span className="legend-color forest" />
+          Forest
+        </div>
 
-      <div>
-        <span className="legend-color water" />
-        Water
-      </div>
+        <div>
+          <span className="legend-color herbaceous" />
+          H.vegetation
+        </div>
 
-      <div>
-        <span className="legend-color urban" />
-        Urban
-      </div>
+        <div>
+          <span className="legend-color water" />
+          Water
+        </div>
 
-      <div>
-        <span className="legend-color bare-soil" />
-        Bare soil
+        <div>
+          <span className="legend-color urban" />
+          Urban
+        </div>
+
+        <div>
+          <span className="legend-color bare-soil" />
+          Bare soil
+        </div>
       </div>
     </div>
   );
