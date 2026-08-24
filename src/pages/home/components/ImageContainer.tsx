e81333002b2c6e998f3362d10e6b19ed3230e3c7
elvis <elviskarl343@gmail.com>
2026-08-24 22:10:05 +0300

    Added citation prop to ImageContainer for displaying citation links

--- src/pages/home/components/ImageContainer.tsx
+++ src/pages/home/components/ImageContainer.tsx
@@ -1,12 +1,24 @@
+import CitationLink from "./CitationLink";
+
 interface ImageContainerProps {
   url: string;
   attr: string;
+  citation?: { id: string; position: number };
 }
-export default function ImageContainer({ attr, url }: ImageContainerProps) {
+export default function ImageContainer({
+  attr,
+  url,
+  citation,
+}: ImageContainerProps) {
   return (
     <div className="image-container">
       <img src={url} alt="forest-blocks" />
-      <span className="attribution">{attr}</span>
+      <span className="attribution">
+        {attr}{" "}
+        {citation && (
+          <CitationLink id={citation.id} position={citation.position} />
+        )}
+      </span>
     </div>
   );
 }
