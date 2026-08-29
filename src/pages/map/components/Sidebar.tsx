864b93628c5b41fbf6441970cfb5b828c1daeb99
elvis <elviskarl343@gmail.com>
2026-08-30 00:33:14 +0300

    Implement collapsible sidebar functionality and update styles

--- src/pages/map/components/Sidebar.tsx
+++ src/pages/map/components/Sidebar.tsx
@@ -1,11 +1,15 @@
 import ListItem from "./ListItem";
 import "../styles/sidebar.css";
 import layerImageUrl from "../../../assets/layers-outline.svg";
+import menuImageUrl from "../../../assets/menu-outline.svg";
 import LayersComponent from "./LayersComponent";
+import { useState } from "react";
+import "../styles/sidebarMediaQuery.css";
 
 function Sidebar() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
   return (
-    <aside className="sidebar">
+    <aside className={`sidebar ${isCollapsed ? "sidebar-closed" : ""}`}>
       <section className="sidebar-container">
         <ul className="sidebar-list">
           <ListItem
@@ -15,6 +19,13 @@ function Sidebar() {
           />
         </ul>
       </section>
+      <div
+        className="menu-image-container"
+        onClick={() => setIsCollapsed((prevVal) => !prevVal)}
+        title={isCollapsed ? "Expand" : "Collapse"}
+      >
+        <img src={menuImageUrl} alt="menu" />
+      </div>
     </aside>
   );
 }
